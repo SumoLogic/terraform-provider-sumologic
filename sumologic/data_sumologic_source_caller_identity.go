0ec7ffc657f0cde58af7ed042dc5b39f37c0c759
Frank Reno <frank.reno@me.com>
2019-02-04 08:03:17 -0700

    remove accessKey from caller identity

--- sumologic/data_sumologic_source_caller_identity.go
+++ sumologic/data_sumologic_source_caller_identity.go
@@ -29,7 +29,6 @@ func dataSourceSumologicCallerIdentityRead(d *schema.ResourceData, meta interfac
 
 	d.SetId(time.Now().UTC().String())
 	d.Set("access_id", c.AccessID)
-	d.Set("access_key", c.AccessKey)
 	d.Set("environment", c.Environment)
 
 	return nil
