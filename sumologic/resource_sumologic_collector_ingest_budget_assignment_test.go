3e0761db338fef9b572d8b18e2a83559059e5950
Will May <will.j.may@gmail.com>
2019-06-19 21:21:27 +0100

    Add support for ingest budgets along with assigning collectors to them

---
+++ sumologic/resource_sumologic_collector_ingest_budget_assignment_test.go
@@ -0,0 +1,38 @@
+package sumologic
+
+import (
+	"github.com/hashicorp/terraform/helper/resource"
+	"testing"
+)
+
+func TestAccSumologicCollectorIngestBudgetAssignment(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSumologicCollectorIngestBudgetAssignmentConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("sumologic_collector_ingest_budget_assignment.assignment", "id"),
+					resource.TestCheckResourceAttrSet("sumologic_collector_ingest_budget_assignment.assignment", "collector_id"),
+					resource.TestCheckResourceAttrSet("sumologic_collector_ingest_budget_assignment.assignment", "ingest_budget_id"),
+				),
+			},
+		}})
+}
+
+var testAccSumologicCollectorIngestBudgetAssignmentConfig = `
+resource "sumologic_collector" "test" {
+  name = "assignment"
+}
+
+resource "sumologic_ingest_budget" "test" {
+  name = "assignment"
+  field_value = "assignment"
+  capacity_bytes = 2
+}
+
+resource "sumologic_collector_ingest_budget_assignment" "assignment" {
+  collector_id = "${sumologic_collector.test.id}"
+  ingest_budget_id = "${sumologic_ingest_budget.test.id}"
+}
+`
