33e87ec3b79dbf1bde760348529b4b7ffdf796a3
Chris Griggs <cgriggs@hashicorp.com>
2020-04-09 09:16:57 -0700

    update repo name

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/terraform-providers/sumologic-terraform-provider/sumologic"
+	"github.com/terraform-providers/terraform-provider-sumologic/sumologic"
 )
 
 func main() {
