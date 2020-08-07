795012c99960d673977a733c902c5c0753f0add0
Vijit Singhal <vsinghal@sumologic.com>
2020-08-07 12:38:24 -0700

    add resource: metadata_source

---
+++ sumologic/sumologic_metadata_source.go
@@ -0,0 +1,111 @@
+package sumologic
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type MetadataSource struct {
+	Source
+	ContentType   string                `json:"contentType"`
+	ScanInterval  int                   `json:"scanInterval"`
+	Paused        bool                  `json:"paused"`
+	URL           string                `json:"url"`
+	ThirdPartyRef MetadataThirdPartyRef `json:"thirdPartyRef,omitempty"`
+}
+
+type MetadataThirdPartyRef struct {
+	Resources []MetadataResource `json:"resources"`
+}
+
+type MetadataResource struct {
+	ServiceType    string                 `json:"serviceType"`
+	Authentication MetadataAuthentication `json:"authentication"`
+	Path           MetadataPath           `json:"path"`
+}
+
+type MetadataAuthentication struct {
+	Type    string `json:"type"`
+	AwsID   string `json:"awsId"`
+	AwsKey  string `json:"awsKey"`
+	RoleARN string `json:"roleARN"`
+}
+
+type MetadataPath struct {
+	Type              string   `json:"type"`
+	BucketName        string   `json:"bucketName,omitempty"`
+	PathExpression    string   `json:"pathExpression,omitempty"`
+	LimitToRegions    []string `json:"limitToRegions,omitempty"`
+	LimitToNamespaces []string `json:"limitToNamespaces,omitempty"`
+	TagFilters        []string `json:"tagFilters,omitempty"`
+}
+
+func (s *Client) CreateMetadataSource(source MetadataSource, collectorID int) (int, error) {
+
+	type MetadataSourceMessage struct {
+		Source MetadataSource `json:"source"`
+	}
+
+	request := MetadataSourceMessage{
+		Source: source,
+	}
+
+	urlPath := fmt.Sprintf("v1/collectors/%d/sources", collectorID)
+
+	body, err := s.Post(urlPath, request)
+
+	if err != nil {
+		return -1, err
+	}
+
+	var response MetadataSourceMessage
+	err = json.Unmarshal(body, &response)
+
+	if err != nil {
+		return -1, err
+	}
+
+	return response.Source.ID, nil
+}
+
+func (s *Client) GetMetadataSource(collectorID, sourceID int) (*MetadataSource, error) {
+	urlPath := fmt.Sprintf("v1/collectors/%d/sources/%d", collectorID, sourceID)
+	body, _, err := s.Get(urlPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if body == nil {
+		return nil, nil
+	}
+
+	type MetadataSourceResponse struct {
+		Source MetadataSource `json:"source"`
+	}
+
+	var response MetadataSourceResponse
+	err = json.Unmarshal(body, &response)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &response.Source, nil
+}
+
+func (s *Client) UpdateMetadataSource(source MetadataSource, collectorID int) error {
+	url := fmt.Sprintf("v1/collectors/%d/sources/%d", collectorID, source.ID)
+
+	type MetadataSourceMessage struct {
+		Source MetadataSource `json:"source"`
+	}
+
+	request := MetadataSourceMessage{
+		Source: source,
+	}
+
+	_, err := s.Put(url, request)
+
+	return err
+}
