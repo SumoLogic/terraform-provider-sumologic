aafe933de2d1d959e425d1c89db5acff788df789
Rohit Sanbhadti <rohit@sumologic.com>
2020-09-03 18:24:50 -0700

    removed local changes to support nite/ephemeral

--- sumologic/sumologic_client.go
+++ sumologic/sumologic_client.go
@@ -25,15 +25,13 @@ type Client struct {
 }
 
 var endpoints = map[string]string{
-	"nite":    "https://nite-api.sumologic.net/api/",
-	"penguin": "https://penguin-api.ephemeral.in/api/",
-	"us1":     "https://api.sumologic.com/api/",
-	"us2":     "https://api.us2.sumologic.com/api/",
-	"eu":      "https://api.eu.sumologic.com/api/",
-	"au":      "https://api.au.sumologic.com/api/",
-	"de":      "https://api.de.sumologic.com/api/",
-	"jp":      "https://api.jp.sumologic.com/api/",
-	"ca":      "https://api.ca.sumologic.com/api/",
+	"us1": "https://api.sumologic.com/api/",
+	"us2": "https://api.us2.sumologic.com/api/",
+	"eu":  "https://api.eu.sumologic.com/api/",
+	"au":  "https://api.au.sumologic.com/api/",
+	"de":  "https://api.de.sumologic.com/api/",
+	"jp":  "https://api.jp.sumologic.com/api/",
+	"ca":  "https://api.ca.sumologic.com/api/",
 }
 
 var rateLimiter = time.NewTicker(time.Minute / 240)
