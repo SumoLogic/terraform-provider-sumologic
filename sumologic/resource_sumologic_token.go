aeb86e38c4bd41ce14b77ee97627b7c6ba14bd8a
Yuting Liu <yuting@sumologic.com>
2021-06-10 10:21:32 -0700

    Add Terraform support for token

---
+++ sumologic/resource_sumologic_token.go
@@ -0,0 +1,118 @@
+package sumologic
+
+import (
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceSumologicToken() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSumologicTokenCreate,
+		Read:   resourceSumologicTokenRead,
+		Update: resourceSumologicTokenUpdate,
+		Delete: resourceSumologicTokenDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"version": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+
+			"status": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"type": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceSumologicTokenCreate(d *schema.ResourceData, meta interface{}) error {
+	c := meta.(*Client)
+
+	if d.Id() == "" {
+		token := resourceToToken(d)
+		id, err := c.CreateToken(token)
+		if err != nil {
+			return err
+		}
+
+		d.SetId(id)
+	}
+
+	return resourceSumologicTokenRead(d, meta)
+}
+
+func resourceSumologicTokenRead(d *schema.ResourceData, meta interface{}) error {
+	c := meta.(*Client)
+
+	id := d.Id()
+	token, err := c.GetToken(id)
+	if err != nil {
+		return err
+	}
+
+	if token == nil {
+		log.Printf("[WARN] Token not found, removing from state: %v - %v", id, err)
+		d.SetId("")
+		return nil
+	}
+
+	d.Set("name", token.Name)
+	d.Set("status", token.Status)
+	d.Set("description", token.Description)
+	d.Set("version", token.Version)
+	d.Set("type", token.Type)
+
+	return nil
+}
+
+func resourceSumologicTokenDelete(d *schema.ResourceData, meta interface{}) error {
+	c := meta.(*Client)
+
+	return c.DeleteToken(d.Id())
+}
+
+func resourceSumologicTokenUpdate(d *schema.ResourceData, meta interface{}) error {
+	c := meta.(*Client)
+
+	token := resourceToToken(d)
+	err := c.UpdateToken(token)
+	if err != nil {
+		return err
+	}
+	// Each successful update will increment version
+	d.Set("version", token.Version+1)
+	return resourceSumologicTokenRead(d, meta)
+}
+
+func resourceToToken(d *schema.ResourceData) Token {
+
+	return Token{
+		Name:        d.Get("name").(string),
+		ID:          d.Id(),
+		Description: d.Get("description").(string),
+		Version:     d.Get("version").(int),
+		Type:        d.Get("type").(string),
+		Status:      d.Get("status").(string),
+	}
+}
---
+++ sumologic/sumologic_token.go
@@ -0,0 +1,90 @@
+package sumologic
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func (s *Client) CreateToken(token Token) (string, error) {
+	urlWithoutParams := "v1/tokens"
+
+	data, err := s.Post(urlWithoutParams, token)
+	if err != nil {
+		return "", err
+	}
+
+	var createdToken Token
+
+	err = json.Unmarshal(data, &createdToken)
+	if err != nil {
+		return "", err
+	}
+
+	return createdToken.ID, nil
+
+}
+
+func (s *Client) GetToken(id string) (*Token, error) {
+	urlWithoutParams := "v1/tokens/%s"
+	paramString := ""
+	sprintfArgs := []interface{}{}
+	sprintfArgs = append(sprintfArgs, id)
+
+	urlWithParams := fmt.Sprintf(urlWithoutParams+paramString, sprintfArgs...)
+
+	data, _, err := s.Get(urlWithParams)
+	if err != nil {
+		return nil, err
+	}
+	if data == nil {
+		return nil, nil
+	}
+
+	var token Token
+
+	err = json.Unmarshal(data, &token)
+	if err != nil {
+		return nil, err
+	}
+
+	return &token, nil
+
+}
+
+func (s *Client) DeleteToken(id string) error {
+	urlWithoutParams := "v1/tokens/%s"
+	paramString := ""
+	sprintfArgs := []interface{}{}
+	sprintfArgs = append(sprintfArgs, id)
+
+	urlWithParams := fmt.Sprintf(urlWithoutParams+paramString, sprintfArgs...)
+
+	_, err := s.Delete(urlWithParams)
+
+	return err
+}
+
+func (s *Client) UpdateToken(token Token) error {
+	urlWithoutParams := "v1/tokens/%s"
+	paramString := ""
+	sprintfArgs := []interface{}{}
+	sprintfArgs = append(sprintfArgs, token.ID)
+
+	urlWithParams := fmt.Sprintf(urlWithoutParams+paramString, sprintfArgs...)
+
+	token.ID = ""
+
+	_, err := s.Put(urlWithParams, token)
+
+	return err
+
+}
+
+type Token struct {
+	Name        string `json:"name"`
+	Status      string `json:"status"`
+	ID          string `json:"id,omitempty"`
+	Description string `json:"description"`
+	Type        string `json:"type"`
+	Version     int    `json:"version"`
+}
