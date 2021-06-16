2d5ec2801756ec5a7a2d9276a1d380d6c5884509
Srujana <srujana@sumologic.com>
2021-06-16 12:01:28 -0700

    SUMO-166948: check

--- sumologic/resource_sumologic_saml_configuration_test.go
+++ sumologic/resource_sumologic_saml_configuration_test.go
@@ -56,6 +56,7 @@ func TestAccSumologicSamlConfiguration_basic(t *testing.T) {
 		SignAuthnRequest:             false,
 		DisableRequestedAuthnContext: false,
 		IsRedirectBinding:            false,
+		AssertionConsumerUrl:         "srujana",
 	}
 
 	resource.Test(t, resource.TestCase{
