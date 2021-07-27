0193377fdf5439ad5f821bfc464cf0c50d1a1949
Rohit Sanbhadti <rohit@sumologic.com>
2021-07-26 20:33:34 -0700

    SUMO-170407: updated connection schema

--- sumologic/resource_sumologic_connection.go
+++ sumologic/resource_sumologic_connection.go
@@ -77,8 +77,14 @@ func resourceSumologicConnection() *schema.Resource {
 			"webhook_type": {
 				Type:         schema.TypeString,
 				Optional:     true,
-				ValidateFunc: validation.StringInSlice([]string{"AWSLambda", "Azure", "Datadog", "HipChat", "PagerDuty", "Slack", "Webhook", "NewRelic", "Jira", "Opsgenie", "MicrosoftTeams"}, false),
+				ValidateFunc: validation.StringInSlice([]string{"AWSLambda", "Azure", "Datadog", "HipChat", "PagerDuty", "Slack", "Webhook", "NewRelic", "Jira", "Opsgenie", "MicrosoftTeams", "ServiceNow"}, false),
 				Default:      "Webhook",
+			},
+			"connection_subtype": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringInSlice([]string{"Incident", "Event", ""}, false),
+				Default:      "",
 			},
 		},
 	}
@@ -136,6 +142,7 @@ func resourceSumologicConnectionRead(d *schema.ResourceData, meta interface{}) e
 	}
 	d.Set("default_payload", connection.DefaultPayload)
 	d.Set("webhook_type", connection.WebhookType)
+	d.Set("connection_subtype", connection.ConnectionSubtype)
 	d.SetId(connection.ID)
 
 	log.Println("====End Connection Read====")
@@ -200,8 +207,8 @@ func resourceToConnection(d *schema.ResourceData) Connection {
 	connection.Headers = mapToHeaders(d.Get("headers").(map[string]interface{}))
 	connection.CustomHeaders = mapToHeaders(d.Get("custom_headers").(map[string]interface{}))
 	connection.DefaultPayload = d.Get("default_payload").(string)
-	connection.ConnectionSubtype = d.Get("connection_subtype").(string)
 	connection.WebhookType = d.Get("webhook_type").(string)
+	connection.ConnectionSubtype = d.Get("connection_subtype").(string)
 
 	return connection
 }
@@ -234,4 +241,5 @@ func printConnection(connection Connection) {
 	log.Printf("CustomHeaders: %s", connection.CustomHeaders)
 	log.Printf("DefaultPayload: %s", connection.DefaultPayload)
 	log.Printf("WebhookType: %s", connection.WebhookType)
+	log.Printf("ConnectionSubtype: %s", connection.ConnectionSubtype)
 }
