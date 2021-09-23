153d64507e3a8704ac7de47c00d1dce310653e99
Pedro Montiel <pmontiel.ctr@sumologic.com>
2021-09-22 22:06:55 -0500

    resolve merge conflict with latest master

--- sumologic/provider.go
+++ sumologic/provider.go
@@ -43,6 +43,7 @@ func Provider() terraform.ResourceProvider {
 			},
 		},
 		ResourcesMap: map[string]*schema.Resource{
+			"sumologic_cse_rule_tuning_expression":         resourceSumologicCSERuleTuningExpression(),
 			"sumologic_cse_network_block":                  resourceSumologicCSENetworkBlock(),
 			"sumologic_collector":                          resourceSumologicCollector(),
 			"sumologic_http_source":                        resourceSumologicHTTPSource(),
