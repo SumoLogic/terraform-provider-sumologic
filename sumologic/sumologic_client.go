20d88df713a553639bbbac109568a16c45d2731d
Pedro Montiel <pmontiel.ctr@sumologic.com>
2022-02-26 16:48:37 -0600

    commit by mistake

--- sumologic/sumologic_client.go
+++ sumologic/sumologic_client.go
@@ -2,15 +2,17 @@ package sumologic
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"time"
+
+	"github.com/hashicorp/go-retryablehttp"
 )
 
 type HttpClient interface {
@@ -20,6 +22,7 @@ type HttpClient interface {
 type Client struct {
 	AccessID      string
 	AccessKey     string
+	AuthJwt       string
 	Environment   string
 	BaseURL       *url.URL
 	IsInAdminMode bool
@@ -29,29 +32,31 @@ type Client struct {
 var ProviderVersion string
 
 var endpoints = map[string]string{
-	"us1":  "https://api.sumologic.com/api/",
-	"us2":  "https://api.us2.sumologic.com/api/",
-	"fed":  "https://api.fed.sumologic.com/api/",
-	"eu":   "https://api.eu.sumologic.com/api/",
-	"au":   "https://api.au.sumologic.com/api/",
-	"de":   "https://api.de.sumologic.com/api/",
-	"jp":   "https://api.jp.sumologic.com/api/",
-	"ca":   "https://api.ca.sumologic.com/api/",
-	"in":   "https://api.in.sumologic.com/api/",
-	"nite": "https://stag-api.sumologic.net/api/",
-	"stag": "https://stag-api.sumologic.net/api/",
+	"us1": "https://api.sumologic.com/api/",
+	"us2": "https://api.us2.sumologic.com/api/",
+	"fed": "https://api.fed.sumologic.com/api/",
+	"eu":  "https://api.eu.sumologic.com/api/",
+	"au":  "https://api.au.sumologic.com/api/",
+	"de":  "https://api.de.sumologic.com/api/",
+	"jp":  "https://api.jp.sumologic.com/api/",
+	"ca":  "https://api.ca.sumologic.com/api/",
+	"in":  "https://api.in.sumologic.com/api/",
 }
 
 var rateLimiter = time.NewTicker(time.Minute / 240)
 
-func createNewRequest(method, url string, body io.Reader, accessID string, accessKey string) (*http.Request, error) {
+func createNewRequest(method, url string, body io.Reader, accessID string, accessKey string, authJwt string) (*http.Request, error) {
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("User-Agent", "SumoLogicTerraformProvider/"+ProviderVersion)
-	req.SetBasicAuth(accessID, accessKey)
+	if authJwt == "" {
+		req.SetBasicAuth(accessID, accessKey)
+	} else {
+		req.Header.Add("Authorization", "Bearer "+authJwt)
+	}
 	return req, nil
 }
 
@@ -68,7 +73,7 @@ func (s *Client) PostWithCookies(urlPath string, payload interface{}) ([]byte, [
 		return nil, nil, err
 	}
 
-	req, err := createNewRequest(http.MethodPost, sumoURL.String(), bytes.NewBuffer(body), s.AccessID, s.AccessKey)
+	req, err := createNewRequest(http.MethodPost, sumoURL.String(), bytes.NewBuffer(body), s.AccessID, s.AccessKey, s.AuthJwt)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -102,7 +107,7 @@ func (s *Client) GetWithCookies(urlPath string, cookies []*http.Cookie) ([]byte,
 
 	sumoURL := s.BaseURL.ResolveReference(relativeURL)
 
-	req, err := createNewRequest(http.MethodGet, sumoURL.String(), nil, s.AccessID, s.AccessKey)
+	req, err := createNewRequest(http.MethodGet, sumoURL.String(), nil, s.AccessID, s.AccessKey, s.AuthJwt)
 	if err != nil {
 		return nil, "", err
 	}
@@ -136,11 +141,8 @@ func (s *Client) Post(urlPath string, payload interface{}) ([]byte, error) {
 	relativeURL, _ := url.Parse(urlPath)
 	sumoURL := s.BaseURL.ResolveReference(relativeURL)
 
-	log.Printf("[INFO] Post URL: %s", sumoURL)
 	body, _ := json.Marshal(payload)
-
-	log.Printf("[INFO] POST body: %s", body)
-	req, err := createNewRequest(http.MethodPost, sumoURL.String(), bytes.NewBuffer(body), s.AccessID, s.AccessKey)
+	req, err := createNewRequest(http.MethodPost, sumoURL.String(), bytes.NewBuffer(body), s.AccessID, s.AccessKey, s.AuthJwt)
 	if err != nil {
 		return nil, err
 	}
@@ -171,9 +173,13 @@ func (s *Client) Post(urlPath string, payload interface{}) ([]byte, error) {
 func (s *Client) PostRawPayload(urlPath string, payload string) ([]byte, error) {
 	relativeURL, _ := url.Parse(urlPath)
 	sumoURL := s.BaseURL.ResolveReference(relativeURL)
-	req, err := createNewRequest(http.MethodPost, sumoURL.String(), bytes.NewBuffer([]byte(payload)), s.AccessID, s.AccessKey)
+	req, err := createNewRequest(http.MethodPost, sumoURL.String(), bytes.NewBuffer([]byte(payload)), s.AccessID, s.AccessKey, s.AuthJwt)
 	if err != nil {
 		return nil, err
+	}
+
+	if s.IsInAdminMode {
+		req.Header.Add("isAdminMode", "true")
 	}
 
 	<-rateLimiter.C
@@ -196,13 +202,10 @@ func (s *Client) Put(urlPath string, payload interface{}) ([]byte, error) {
 	relativeURL, _ := url.Parse(urlPath)
 	sumoURL := s.BaseURL.ResolveReference(relativeURL)
 
-	log.Printf("[INFO] Put URL: %s", sumoURL)
 	_, etag, _ := s.Get(sumoURL.String())
 
 	body, _ := json.Marshal(payload)
-
-	log.Printf("[INFO] PUT body: %s", body)
-	req, err := createNewRequest(http.MethodPut, sumoURL.String(), bytes.NewBuffer(body), s.AccessID, s.AccessKey)
+	req, err := createNewRequest(http.MethodPut, sumoURL.String(), bytes.NewBuffer(body), s.AccessID, s.AccessKey, s.AuthJwt)
 	if err != nil {
 		return nil, err
 	}
@@ -234,8 +237,8 @@ func (s *Client) Put(urlPath string, payload interface{}) ([]byte, error) {
 func (s *Client) Get(urlPath string) ([]byte, string, error) {
 	relativeURL, _ := url.Parse(urlPath)
 	sumoURL := s.BaseURL.ResolveReference(relativeURL)
-	log.Printf("[INFO] Get URL: %s", sumoURL)
-	req, err := createNewRequest(http.MethodGet, sumoURL.String(), nil, s.AccessID, s.AccessKey)
+
+	req, err := createNewRequest(http.MethodGet, sumoURL.String(), nil, s.AccessID, s.AccessKey, s.AuthJwt)
 	if err != nil {
 		return nil, "", err
 	}
@@ -255,7 +258,7 @@ func (s *Client) Get(urlPath string) ([]byte, string, error) {
 	if err != nil {
 		return nil, "", err
 	}
-	log.Printf("[INFO] Get response: %s", string(d))
+
 	if resp.StatusCode == 404 {
 		return nil, "", nil
 	} else if resp.StatusCode >= 400 {
@@ -268,9 +271,8 @@ func (s *Client) Get(urlPath string) ([]byte, string, error) {
 func (s *Client) Delete(urlPath string) ([]byte, error) {
 	relativeURL, _ := url.Parse(urlPath)
 	sumoURL := s.BaseURL.ResolveReference(relativeURL)
-	log.Printf("[INFO] Delete URL: %s", sumoURL)
 
-	req, err := createNewRequest(http.MethodDelete, sumoURL.String(), nil, s.AccessID, s.AccessKey)
+	req, err := createNewRequest(http.MethodDelete, sumoURL.String(), nil, s.AccessID, s.AccessKey, s.AuthJwt)
 	if err != nil {
 		return nil, err
 	}
@@ -298,11 +300,26 @@ func (s *Client) Delete(urlPath string) ([]byte, error) {
 	return d, nil
 }
 
-func NewClient(accessID, accessKey, environment, base_url string, admin bool) (*Client, error) {
+func checkRetry(ctx context.Context, resp *http.Response, err error) (bool, error) {
+	// only retry on 429
+	if err == nil && resp.StatusCode == http.StatusTooManyRequests {
+		return true, nil
+	}
+	return false, nil
+}
+
+func NewClient(accessID, accessKey, authJwt, environment, base_url string, admin bool) (*Client, error) {
+	retryClient := retryablehttp.NewClient()
+	retryClient.RetryMax = 10
+	retryClient.CheckRetry = checkRetry
+	// Disable DEBUG logs (https://github.com/hashicorp/go-retryablehttp/issues/31)
+	retryClient.Logger = nil
+
 	client := Client{
 		AccessID:      accessID,
 		AccessKey:     accessKey,
-		httpClient:    http.DefaultClient,
+		AuthJwt:       authJwt,
+		httpClient:    retryClient.StandardClient(),
 		Environment:   environment,
 		IsInAdminMode: admin,
 	}
