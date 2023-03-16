1f374822676ac8af227c253657c3f757a905d7db
Jayesh Inkane <jinkane@sumologic.com>
2023-03-16 13:55:30 +0530

    SUMO-213711 added exmaple for slo trigger conditions in acceptance test

--- sumologic/sumologic_monitors_library_monitor.go
+++ sumologic/sumologic_monitors_library_monitor.go
@@ -199,7 +199,7 @@ type WebhookNotificiation struct {
 }
 
 type BurnRate struct {
-	BurnRateThreshold float64 `json:"burnRateThreshold,omitempty"`
+	BurnRateThreshold float64 `json:"burnRateThreshold"`
 	TimeRange         string  `json:"timeRange"`
 }
 
