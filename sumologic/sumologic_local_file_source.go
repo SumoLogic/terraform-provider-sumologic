20f9a37b50b1fd65fc2fe29d519fb4c1328c8eaa
Vijit Singhal <vsinghal@sumologic.com>
2023-03-22 18:55:21 -0700

    Add local file source for installed collector (#477)
    * Add local file source for installed collector
    
    * Add docs for local file source
    
    * fix docs
    
    * fix docs indent

---
+++ sumologic/sumologic_local_file_source.go
@@ -0,0 +1,81 @@
+package sumologic
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type LocalFileSource struct {
+	Source
+	PathExpression string   `json:"pathExpression,omitempty"`
+	Encoding       string   `json:"encoding,omitempty"`
+	DenyList       []string `json:"denylist,omitempty"`
+}
+
+func (s *Client) CreateLocalFileSource(source LocalFileSource, collectorID int) (int, error) {
+
+	type LocalFileSourceMessage struct {
+		Source LocalFileSource `json:"source"`
+	}
+
+	request := LocalFileSourceMessage{
+		Source: source,
+	}
+
+	urlPath := fmt.Sprintf("v1/collectors/%d/sources", collectorID)
+	body, err := s.Post(urlPath, request)
+
+	if err != nil {
+		return -1, err
+	}
+
+	var response LocalFileSourceMessage
+
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return -1, err
+	}
+
+	return response.Source.ID, nil
+}
+
+func (s *Client) GetLocalFileSource(collectorID, sourceID int) (*LocalFileSource, error) {
+
+	body, _, err := s.Get(fmt.Sprintf("v1/collectors/%d/sources/%d", collectorID, sourceID))
+	if err != nil {
+		return nil, err
+	}
+
+	if body == nil {
+		return nil, nil
+	}
+
+	type LocalFileSourceResponse struct {
+		Source LocalFileSource `json:"source"`
+	}
+
+	var response LocalFileSourceResponse
+
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response.Source, nil
+}
+
+func (s *Client) UpdateLocalFileSource(source LocalFileSource, collectorID int) error {
+
+	type LocalFileSourceMessage struct {
+		Source LocalFileSource `json:"source"`
+	}
+
+	request := LocalFileSourceMessage{
+		Source: source,
+	}
+
+	urlPath := fmt.Sprintf("v1/collectors/%d/sources/%d", collectorID, source.ID)
+	_, err := s.Put(urlPath, request)
+
+	return err
+}
