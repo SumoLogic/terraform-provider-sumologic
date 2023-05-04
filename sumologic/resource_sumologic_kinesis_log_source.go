42d31ad5a6e61609a983d436e45676def829d0cf
Yuting Liu <yuting@sumologic.com>
2023-05-04 16:18:18 -0700

    SUMO-218762 Make authentication and path required for kinesis log source

--- sumologic/resource_sumologic_kinesis_log_source.go
+++ sumologic/resource_sumologic_kinesis_log_source.go
@@ -36,7 +36,7 @@ func resourceSumologicKinesisLogSource() *schema.Resource {
 	}
 	kinesisLogSource.Schema["authentication"] = &schema.Schema{
 		Type:     schema.TypeList,
-		Optional: true,
+		Required: true,
 		MaxItems: 1,
 		Elem: &schema.Resource{
 			Schema: map[string]*schema.Schema{
@@ -63,7 +63,7 @@ func resourceSumologicKinesisLogSource() *schema.Resource {
 
 	kinesisLogSource.Schema["path"] = &schema.Schema{
 		Type:     schema.TypeList,
-		Optional: true,
+		Required: true,
 		MaxItems: 1,
 		Elem: &schema.Resource{
 			Schema: map[string]*schema.Schema{
