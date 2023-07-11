2565d729049e65f7280a955e14445bdfdf0f93a2
Damian Kluba <dkluba@sumologic.com>
2023-07-11 13:15:14 +0200

    TRACING-3869 Add Create/Get/Update methods for RumSource

--- sumologic/resource_sumologic_rum_source.go
+++ sumologic/resource_sumologic_rum_source.go
@@ -1,5 +1,10 @@
 package sumologic
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type RumSource struct {
 	HTTPSource
 	RumThirdPartyRef RumThirdPartyRef `json:"thirdPartyRef"`
@@ -25,4 +30,72 @@ type RumSourcePath struct {
 	CustomTags                   map[string]interface{} `json:"customTags"`
 	PropagateTraceHeaderCorsUrls []string               `json:"propagateTraceHeaderCorsUrls,omitempty"`
 	SelectedCountry              string                 `json:"selectedCountry,omitempty"`
+}
+
+func (s *Client) CreateRumSource(rumSource RumSource, collectorID int) (int, error) {
+
+	type RumSourceMessage struct {
+		Source RumSource `json:"source"`
+	}
+
+	request := RumSourceMessage{
+		Source: rumSource,
+	}
+
+	urlPath := fmt.Sprintf("v1/collectors/%d/sources", collectorID)
+	body, err := s.Post(urlPath, request)
+
+	if err != nil {
+		return -1, err
+	}
+
+	var response RumSourceMessage
+
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return -1, err
+	}
+
+	return response.Source.ID, nil
+}
+
+func (s *Client) GetRumSource(collectorID, sourceID int) (*RumSource, error) {
+
+	body, _, err := s.Get(fmt.Sprintf("v1/collectors/%d/sources/%d", collectorID, sourceID))
+	if err != nil {
+		return nil, err
+	}
+
+	if body == nil {
+		return nil, nil
+	}
+
+	type Response struct {
+		Source RumSource `json:"source"`
+	}
+
+	var response Response
+
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response.Source, nil
+}
+
+func (s *Client) UpdateRumSource(source RumSource, collectorID int) error {
+
+	type RumSourceMessage struct {
+		Source RumSource `json:"source"`
+	}
+
+	request := RumSourceMessage{
+		Source: source,
+	}
+
+	urlPath := fmt.Sprintf("v1/collectors/%d/sources/%d", collectorID, source.ID)
+	_, err := s.Put(urlPath, request)
+
+	return err
 }
