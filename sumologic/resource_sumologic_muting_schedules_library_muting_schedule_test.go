1e4a0431362220ac00e4136f9805bd0dd567f3ea
James Xie <jxie@sumologic.com>
2023-08-28 14:37:38 -0700

    SUMO-226102:added TF support for muting schedule

---
+++ sumologic/resource_sumologic_muting_schedules_library_muting_schedule_test.go
@@ -0,0 +1,313 @@
+package sumologic
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccSumologicMutingSchedulesLibraryMutingSchedule_basic(t *testing.T) {
+	var mutingSchedulesLibraryMutingSchedule MutingSchedulesLibraryMutingSchedule
+	testNameSuffix := acctest.RandString(16)
+
+	testName := "terraform_test_muting_schedule_" + testNameSuffix
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckMutingSchedulesLibraryMutingScheduleDestroy(mutingSchedulesLibraryMutingSchedule),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSumologicMutingSchedulesLibraryMutingSchedule(testName),
+			},
+			{
+				ResourceName:      "sumologic_muting_schedule.test",
+				ImportState:       true,
+				ImportStateVerify: false,
+			},
+		},
+	})
+}
+func TestAccSumologicMutingSchedulesLibraryMutingSchedule_create(t *testing.T) {
+	var mutingSchedulesLibraryMutingSchedule MutingSchedulesLibraryMutingSchedule
+	testNameSuffix := acctest.RandString(16)
+	tomorrow := time.Now().AddDate(0, 0, 1)
+
+	testName := "terraform_test_muting_schedule_" + testNameSuffix
+	testDescription := "terraform_test_muting_schedule_description"
+	testType := "MutingSchedulesLibraryMutingSchedule"
+	testContentType := "MutingSchedule"
+	testMonitor := MonitorScope{
+		All: true,
+	}
+	testSchedule := ScheduleDefinition{
+		TimeZone:  "America/Los_Angeles",
+		StartDate: tomorrow.Format("2006-01-02"),
+		StartTime: "00:00",
+		Duration:  40,
+		RRule:     "FREQ=DAILY;INTERVAL=1;BYHOUR=9,10",
+		IsForm:    false,
+	}
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckMutingSchedulesLibraryMutingScheduleDestroy(mutingSchedulesLibraryMutingSchedule),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSumologicMutingSchedulesLibraryMutingSchedule(testNameSuffix),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckMutingSchedulesLibraryMutingScheduleExists("sumologic_muting_schedule.test", &mutingSchedulesLibraryMutingSchedule, t),
+					testAccCheckMutingSchedulesLibraryMutingScheduleAttributes("sumologic_muting_schedule.test"),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "name", testName),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "type", testType),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "description", testDescription),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "content_type", testContentType),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "monitor.0.all", strconv.FormatBool(testMonitor.All)),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.timezone", testSchedule.TimeZone),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.start_date", testSchedule.StartDate),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.start_time", testSchedule.StartTime),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.rrule", testSchedule.RRule),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.is_form", strconv.FormatBool(testSchedule.IsForm)),
+				),
+			},
+		},
+	})
+}
+
+func TestAccSumologicMutingSchedulesLibraryMutingSchedule_update(t *testing.T) {
+	var mutingSchedulesLibraryMutingSchedule MutingSchedulesLibraryMutingSchedule
+	testNameSuffix := acctest.RandString(16)
+	tomorrow := time.Now().AddDate(0, 0, 1)
+	testName := "terraform_test_muting_schedule_" + testNameSuffix
+	testDescription := "terraform_test_muting_schedule_description"
+	testType := "MutingSchedulesLibraryMutingSchedule"
+	testContentType := "MutingSchedule"
+
+	testMonitor := MonitorScope{
+		All: true,
+	}
+	testSchedule := ScheduleDefinition{
+		TimeZone:  "America/Los_Angeles",
+		StartDate: tomorrow.Format("2006-01-02"),
+		StartTime: "00:00",
+		Duration:  40,
+		RRule:     "FREQ=DAILY;INTERVAL=1;BYHOUR=9,10",
+		IsForm:    false,
+	}
+
+	// updated fields
+	testUpdatedName := "terraform_test_muting_schedule_" + testNameSuffix
+	testUpdatedDescription := "terraform_test_muting_schedule_description"
+	testUpdatedType := "MutingSchedulesLibraryMutingSchedule"
+	testUpdatedContentType := "MutingSchedule"
+	testUpdateMonitor := MonitorScope{
+		All: true,
+	}
+	testUpdateSchedule := ScheduleDefinition{
+		TimeZone:  "America/Los_Angeles",
+		StartDate: tomorrow.Format("2006-01-02"),
+		StartTime: "01:00",
+		Duration:  50,
+		RRule:     "FREQ=DAILY;INTERVAL=1",
+		IsForm:    false,
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckMutingSchedulesLibraryMutingScheduleDestroy(mutingSchedulesLibraryMutingSchedule),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSumologicMutingSchedulesLibraryMutingSchedule(testNameSuffix),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckMutingSchedulesLibraryMutingScheduleExists("sumologic_muting_schedule.test", &mutingSchedulesLibraryMutingSchedule, t),
+					testAccCheckMutingSchedulesLibraryMutingScheduleAttributes("sumologic_muting_schedule.test"),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "name", testName),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "type", testType),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "description", testDescription),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "content_type", testContentType),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "monitor.0.all", strconv.FormatBool(testMonitor.All)),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.timezone", testSchedule.TimeZone),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.start_date", testSchedule.StartDate),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.start_time", testSchedule.StartTime),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.rrule", testSchedule.RRule),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.is_form", strconv.FormatBool(testSchedule.IsForm)),
+				),
+			},
+			{
+				Config: testAccSumologicMutingSchedulesLibraryMutingScheduleUpdate(testNameSuffix),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "name", testUpdatedName),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "type", testUpdatedType),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "description", testUpdatedDescription),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "content_type", testUpdatedContentType),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "monitor.0.all", strconv.FormatBool(testUpdateMonitor.All)),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.timezone", testUpdateSchedule.TimeZone),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.start_date", testUpdateSchedule.StartDate),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.start_time", testUpdateSchedule.StartTime),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.duration", strconv.FormatInt((int64)(testUpdateSchedule.Duration), 10)),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.rrule", testUpdateSchedule.RRule),
+					resource.TestCheckResourceAttr("sumologic_muting_schedule.test", "schedule.0.is_form", strconv.FormatBool(testUpdateSchedule.IsForm)),
+				),
+			},
+		},
+	})
+}
+
+func TestAccSumologicMutingSchedulesLibraryMutingSchedule_monitorScopeValidations(t *testing.T) {
+	var mutingSchedulesLibraryMutingSchedule MutingSchedulesLibraryMutingSchedule
+	testNameSuffix := acctest.RandString(16)
+	testName := "terraform_test_muting_schedule_" + testNameSuffix
+	config := testAccSumologicMutingSchedulesLibraryMutingScheduleBadMonitorScope(testName)
+	expectedError := regexp.MustCompile("An argument named \"abc\" is not expected here. Did you mean \"all\"?")
+	resource.Test(t, resource.TestCase{
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckMutingSchedulesLibraryMutingScheduleDestroy(mutingSchedulesLibraryMutingSchedule),
+		Steps: []resource.TestStep{
+			{
+				Config:      config,
+				PlanOnly:    true,
+				ExpectError: expectedError,
+			},
+		},
+	})
+}
+
+func testAccCheckMutingSchedulesLibraryMutingScheduleDestroy(mutingSchedulesLibraryMutingSchedule MutingSchedulesLibraryMutingSchedule) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*Client)
+		for _, r := range s.RootModule().Resources {
+			id := r.Primary.ID
+			u, err := client.MutingSchedulesRead(id)
+			if err != nil {
+				return fmt.Errorf("Encountered an error: " + err.Error())
+			}
+			if u != nil {
+				return fmt.Errorf("MutingSchedulesLibraryMutingSchedule %s still exists", id)
+			}
+		}
+		return nil
+	}
+}
+
+func testAccCheckMutingSchedulesLibraryMutingScheduleExists(name string, mutingSchedulesLibraryMutingSchedule *MutingSchedulesLibraryMutingSchedule, t *testing.T) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Error = %s. MutingSchedulesLibraryMutingSchedule not found: %s", strconv.FormatBool(ok), name)
+		}
+		if strings.EqualFold(rs.Primary.ID, "") {
+			return fmt.Errorf("MutingSchedulesLibraryMutingSchedule ID is not set")
+		}
+
+		id := rs.Primary.ID
+		client := testAccProvider.Meta().(*Client)
+		newMutingSchedulesLibraryMutingSchedule, err := client.MutingSchedulesRead(id)
+		if err != nil {
+			return fmt.Errorf("MutingSchedulesLibraryMutingSchedule %s not found", id)
+		}
+		mutingSchedulesLibraryMutingSchedule = newMutingSchedulesLibraryMutingSchedule
+		return nil
+	}
+}
+
+func testAccCheckMutingSchedulesLibraryMutingScheduleAttributes(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		f := resource.ComposeTestCheckFunc(
+			resource.TestCheckResourceAttrSet(name, "modified_at"),
+			resource.TestCheckResourceAttrSet(name, "created_by"),
+			resource.TestCheckResourceAttrSet(name, "name"),
+			resource.TestCheckResourceAttrSet(name, "parent_id"),
+			resource.TestCheckResourceAttrSet(name, "is_system"),
+			resource.TestCheckResourceAttrSet(name, "is_mutable"),
+			resource.TestCheckResourceAttrSet(name, "type"),
+			resource.TestCheckResourceAttrSet(name, "version"),
+			resource.TestCheckResourceAttrSet(name, "description"),
+			resource.TestCheckResourceAttrSet(name, "modified_by"),
+			resource.TestCheckResourceAttrSet(name, "created_at"),
+			resource.TestCheckResourceAttrSet(name, "content_type"),
+		)
+		return f(s)
+	}
+}
+
+func testAccSumologicMutingSchedulesLibraryMutingSchedule(testName string) string {
+	tomorrow := time.Now().AddDate(0, 0, 1)
+	startDate := tomorrow.Format("2006-01-02")
+	return fmt.Sprintf(`
+    resource "sumologic_muting_schedule" "test" {
+	name = "terraform_test_muting_schedule_%s"
+	description = "terraform_test_muting_schedule_description"
+	type = "MutingSchedulesLibraryMutingSchedule"
+	content_type = "MutingSchedule"
+	monitor {
+		ids = []
+		all = true
+	  }
+	schedule  {
+		timezone = "America/Los_Angeles"
+		start_date = "%s"
+		start_time = "00:00"
+		duration = 40
+		rrule = "FREQ=DAILY;INTERVAL=1;BYHOUR=9,10"
+        is_form = false
+	  }
+}
+`, testName, startDate)
+}
+
+func testAccSumologicMutingSchedulesLibraryMutingScheduleUpdate(testName string) string {
+	tomorrow := time.Now().AddDate(0, 0, 1)
+	startDate := tomorrow.Format("2006-01-02")
+	return fmt.Sprintf(`
+   resource "sumologic_muting_schedule" "test" {
+	name = "terraform_test_muting_schedule_%s"
+	description = "terraform_test_muting_schedule_description"
+	type = "MutingSchedulesLibraryMutingSchedule"
+	content_type = "MutingSchedule"
+	monitor {
+		ids = []
+		all = true
+	  }
+	schedule  {
+	timezone = "America/Los_Angeles"
+	start_date = "%s"
+	start_time = "01:00"
+	duration = 50
+	rrule = "FREQ=DAILY;INTERVAL=1"
+	is_form = false
+	}
+}
+`, testName, startDate)
+}
+
+func testAccSumologicMutingSchedulesLibraryMutingScheduleBadMonitorScope(testName string) string {
+	tomorrow := time.Now().AddDate(0, 0, 1)
+	startDate := tomorrow.Format("2006-01-02")
+	return fmt.Sprintf(`
+   resource "sumologic_muting_schedule" "test" {
+	name = "terraform_test_muting_schedule_%s"
+	description = "terraform_test_muting_schedule_description"
+	type = "MutingSchedulesLibraryMutingSchedule"
+	content_type = "MutingSchedule"
+	monitor {
+		abc="not right"
+	  }
+	schedule  {
+	timezone = "America/Los_Angeles"
+	start_date = "%s"
+	start_time = "01:00"
+	duration = 50
+	rrule = "FREQ=DAILY;INTERVAL=1"
+	is_form = false
+	}
+}
+`, testName, startDate)
+}
---
+++ website/docs/r/muting_schedule.html.markdown
@@ -0,0 +1,124 @@
+---
+layout: 'sumologic'
+page_title: 'SumoLogic: sumologic_muting_schedule'
+description: |-
+  Provides the ability to create, read, delete, and update muting schedule.
+---
+
+# sumologic_muting_schedule
+
+Provides the ability to create, read, delete, and update [MutingSchedule][1].
+
+## Example One-time Muting Schedule From 12:00 AM To 1:00 AM On 2023-08-05 For All monitor
+
+```hcl
+resource "sumologic_muting_schedule" "muting_schedule" {
+  name = "Muting Schedule For one time"
+  description = "This is an example for one time Muting schedule for all monitor"
+	type = "MutingSchedulesLibraryMutingSchedule"
+	content_type = "MutingSchedule"
+	monitor {
+		all = true
+	  }
+	schedule  {
+		timezone = "America/Los_Angeles"
+		start_date = "2023-08-05"
+		start_time = "00:00"
+		duration = 60
+	}
+}
+```
+
+## Example One-time Muting Schedule From 12:00 AM To 1:00 AM On 2023-08-05 For Specifc Monitor/Folder ids
+
+```hcl
+resource "sumologic_muting_schedule" "muting_schedule" {
+  name = "Muting Schedule For one time"
+  description = "This is an example for one time Muting schedule for all monitor"
+	type = "MutingSchedulesLibraryMutingSchedule"
+	content_type = "MutingSchedule"
+	monitor {
+		ids = ["0000000000200B92"]
+	  }
+	schedule  {
+		timezone = "America/Los_Angeles"
+		start_date = "2023-08-05"
+		start_time = "00:00"
+		duration = 60
+	}
+}
+```
+
+## Example Daily Muting Schedule From 9:00 AM to 9:30 and 10:00 AM to 10:30 AM Since 2023-08-05 For All monitor
+
+```hcl
+resource "sumologic_muting_schedule" "muting_schedule" {
+  name = "Muting Schedule For one time"
+  description = "This is an example for one time Muting schedule for all monitor"
+	type = "MutingSchedulesLibraryMutingSchedule"
+	content_type = "MutingSchedule"
+	monitor {
+		all = true
+	  }
+	schedule  {
+		timezone = "America/Los_Angeles"
+		start_date = "2023-08-05"
+		start_time = "00:00"
+		duration = 30
+    	rrule = "FREQ=DAILY;INTERVAL=1;BYHOUR=9,10"
+	}
+}
+```
+
+## Example Daily Muting Schedule From 9:00 AM to 9:30 and 10:00 AM to 10:30 AM Since 2023-08-05 For Specifc Monitor/Folder ids 
+
+```hcl
+resource "sumologic_muting_schedule" "muting_schedule" {
+  name = "Muting Schedule For one time"
+  description = "This is an example for one time Muting schedule for all monitor"
+	type = "MutingSchedulesLibraryMutingSchedule"
+	content_type = "MutingSchedule"
+	monitor {
+		ids = ["0000000000200B92"]
+	  }
+	schedule  {
+		timezone = "America/Los_Angeles"
+		start_date = "2023-08-05"
+		start_time = "00:00"
+		duration = 30
+    	rrule = "FREQ=DAILY;INTERVAL=1;BYHOUR=9,10"
+	}
+}
+```
+
+## Argument reference
+
+The following arguments are supported:
+
+- `type` - (Optional) The type of object model. Valid value:
+  - `MutingSchedulesLibraryMutingSchedule`
+- `name` - (Required) The name of the muting schedule. The name must be alphanumeric.
+- `description` - (Optional) The description of the muting schedule.
+- `content_type` - (Optional) The type of the content object. Valid value:
+  - `MutingSchedule`
+- `monitor` - (Optional) The monitors which need to put in the muting schedule. see `monitor_scope_type`:
+- `schedule` - (Required) The schedule information. see `schedule_type`.
+
+#### schedule_type
+  - `timezone` - (Required) Time zone for the schedule per
+            [IANA Time Zone Database](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).
+  - `start_date` - (Required) Schedule start date in the format of `yyyy-mm-dd`
+  - `start_time` - (Required) Schedule start time in the format of `hh:mm`
+  - `duration` - (Required) Duration of the muting in minutes
+  - `rrule` - (Optional) RRule (Recurrence Rule) Below are some examples of how to represent recurring events using the RRULE format:
+  A rule occurring on the third Sunday of April would be as follows: `FREQ=YEARLY;BYMONTH=4;BYDAY=SU;BYSETPOS=3`
+  An event occurring on the first and second Monday of October would be specified by the rule: `FREQ=YEARLY;BYMONTH=10;BYDAY=MO;BYSETPOS=1,2`
+  Event that repeats monthly: every 29th of every other month! `FREQ=MONTHLY;INTERVAL=2;BYMONTHDAY=29`
+  (https://freetools.textmagic.com/rrule-generator)
+  - `is_form`- (Optional) A flag identifying if the RRule is created or modified through Form UI. This value is determined by the system and users don't have to define it.
+
+#### monitor_scope_type
+  - `ids` - (Optional) List of monitor Ids in hex. Must be empty if `all` is true.
+  - `all` - (Optional) True if the schedule applies to all monitors
+
+[1]: https://help.sumologic.com/docs/alerts/monitors/muting-schedules/
