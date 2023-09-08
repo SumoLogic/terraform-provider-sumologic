23fa28ab8025e48856b6367001b3f107fe4b3c23
Pedro Montiel <pmontiel.ctr@sumologic.com>
2023-09-08 14:26:06 -0500

    INVS-1625: Support CSE context actions

---
+++ sumologic/resource_sumologic_cse_context_action.go
@@ -0,0 +1,166 @@
+package sumologic
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"log"
+)
+
+func resourceSumologicCSEContextAction() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSumologicCSEContextActionCreate,
+		Read:   resourceSumologicCSEContextActionRead,
+		Delete: resourceSumologicCSEContextActionDelete,
+		Update: resourceSumologicCSEContextActionUpdate,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"type": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type:         schema.TypeString,
+					ValidateFunc: validation.All(validation.StringIsNotEmpty, validation.StringInSlice([]string{"URL", "QUERY"}, false)),
+				},
+			},
+			"template": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"ioc_types": {
+				Type:     schema.TypeList,
+				Required: true,
+				Elem: &schema.Schema{
+					Type:         schema.TypeString,
+					ValidateFunc: validation.All(validation.StringIsNotEmpty, validation.StringInSlice([]string{"ASN", "DOMAIN", "HASH", "IP_ADDRESS", "MAC_ADDRESS", "PORT", "RECORD_PROPERTY", "URL"}, false)),
+				},
+			},
+			"entity_types": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type:         schema.TypeString,
+					ValidateFunc: validation.StringIsNotEmpty,
+				},
+			},
+			"record_fields": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type:         schema.TypeString,
+					ValidateFunc: validation.StringIsNotEmpty,
+				},
+			},
+			"all_record_fields": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+		},
+	}
+}
+
+func resourceSumologicCSEContextActionRead(d *schema.ResourceData, meta interface{}) error {
+	c := meta.(*Client)
+
+	var CSEContextAction *CSEContextAction
+	id := d.Id()
+
+	CSEContextAction, err := c.GetCSEContextAction(id)
+	if err != nil {
+		log.Printf("[WARN] CSE Context Action not found when looking by id: %s, err: %v", id, err)
+
+	}
+
+	if CSEContextAction == nil {
+		log.Printf("[WARN] CSE Context Action not found, removing from state: %v - %v", id, err)
+		d.SetId("")
+		return nil
+	}
+
+	d.Set("name", CSEContextAction.Name)
+	d.Set("type", CSEContextAction.Type)
+	d.Set("template", CSEContextAction.Template)
+	d.Set("ioc_types", CSEContextAction.IocTypes)
+	d.Set("entity_types", CSEContextAction.EntityTypes)
+	d.Set("record_fields", CSEContextAction.RecordFields)
+	d.Set("all_record_fields", CSEContextAction.AllRecordFields)
+	d.Set("enabled", CSEContextAction.Enabled)
+
+	return nil
+}
+
+func resourceSumologicCSEContextActionDelete(d *schema.ResourceData, meta interface{}) error {
+	c := meta.(*Client)
+
+	return c.DeleteCSEContextAction(d.Id())
+
+}
+
+func resourceSumologicCSEContextActionCreate(d *schema.ResourceData, meta interface{}) error {
+	c := meta.(*Client)
+
+	if d.Id() == "" {
+		id, err := c.CreateCSEContextAction(CSEContextAction{
+			Name:            d.Get("name").(string),
+			Type:            d.Get("type").(string),
+			Template:        d.Get("template").(string),
+			IocTypes:        resourceFieldsToStringArray(d.Get("ioc_types").([]interface{})),
+			EntityTypes:     resourceFieldsToStringArray(d.Get("entity_types").([]interface{})),
+			RecordFields:    resourceFieldsToStringArray(d.Get("record_fields").([]interface{})),
+			AllRecordFields: d.Get("all_record_fields").(bool),
+			Enabled:         d.Get("enabled").(bool),
+		})
+
+		if err != nil {
+			return err
+		}
+
+		d.SetId(id)
+	}
+
+	return resourceSumologicCSEContextActionRead(d, meta)
+}
+
+func resourceSumologicCSEContextActionUpdate(d *schema.ResourceData, meta interface{}) error {
+	CSEContextAction, err := resourceToCSEContextAction(d)
+	if err != nil {
+		return err
+	}
+	c := meta.(*Client)
+	if err = c.UpdateCSEContextAction(CSEContextAction); err != nil {
+		return err
+	}
+
+	return resourceSumologicCSEContextActionRead(d, meta)
+}
+
+func resourceToCSEContextAction(d *schema.ResourceData) (CSEContextAction, error) {
+	id := d.Id()
+	if id == "" {
+		return CSEContextAction{}, nil
+	}
+
+	return CSEContextAction{
+		ID:              id,
+		Name:            d.Get("name").(string),
+		Type:            d.Get("type").(string),
+		Template:        d.Get("template").(string),
+		IocTypes:        resourceFieldsToStringArray(d.Get("ioc_types").([]interface{})),
+		EntityTypes:     resourceFieldsToStringArray(d.Get("entity_types").([]interface{})),
+		RecordFields:    resourceFieldsToStringArray(d.Get("record_fields").([]interface{})),
+		AllRecordFields: d.Get("all_record_fields").(bool),
+		Enabled:         d.Get("enabled").(bool),
+	}, nil
+}
---
+++ sumologic/sumologic_cse_context_action.go
@@ -0,0 +1,85 @@
+package sumologic
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func (s *Client) GetCSEContextAction(id string) (*CSEContextAction, error) {
+	data, _, err := s.Get(fmt.Sprintf("sec/v1/context-actions/%s", id))
+	if err != nil {
+		return nil, err
+	}
+
+	if data == nil {
+		return nil, nil
+	}
+
+	var response CSEContextActionResponse
+	err = json.Unmarshal(data, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response.CSEContextAction, nil
+}
+
+func (s *Client) DeleteCSEContextAction(id string) error {
+	_, err := s.Delete(fmt.Sprintf("sec/v1/context-actions/%s", id))
+
+	return err
+}
+
+func (s *Client) CreateCSEContextAction(CSEContextAction CSEContextAction) (string, error) {
+
+	request := CSEContextAction
+
+	var response CSEContextActionResponse
+
+	responseBody, err := s.Post("sec/v1/context-actions", request)
+	if err != nil {
+		return "", err
+	}
+
+	err = json.Unmarshal(responseBody, &response)
+
+	if err != nil {
+		return "", err
+	}
+
+	return response.CSEContextAction.ID, nil
+}
+
+func (s *Client) UpdateCSEContextAction(CSEContextAction CSEContextAction) error {
+	url := fmt.Sprintf("sec/v1/context-actions/%s", CSEContextAction.ID)
+
+	CSEContextAction.ID = ""
+
+	request := CSEContextActionRequestUpdate{
+		CSEContextAction,
+	}
+
+	_, err := s.Put(url, request)
+
+	return err
+}
+
+type CSEContextActionRequestUpdate struct {
+	CSEContextAction CSEContextAction `json:"fields"`
+}
+
+type CSEContextActionResponse struct {
+	CSEContextAction CSEContextAction `json:"data"`
+}
+
+type CSEContextAction struct {
+	ID              string   `json:"id,omitempty"`
+	Name            string   `json:"name,omitempty"`
+	Type            string   `json:"type"`
+	Template        string   `json:"template"`
+	IocTypes        []string `json:"iocTypes"`
+	EntityTypes     []string `json:"entityTypes,omitempty"`
+	RecordFields    []string `json:"recordFields,omitempty"`
+	AllRecordFields bool     `json:"allRecordFields"`
+	Enabled         bool     `json:"enabled"`
+}
