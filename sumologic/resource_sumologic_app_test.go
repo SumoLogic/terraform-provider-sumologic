80000375e3f44d147f2b115d1d6b6a520ac40bca
Sajal Jain <sjain@sumologic.com>
2024-01-19 00:18:17 -0800

    SUMO-216512: Add test for resource

---
+++ sumologic/resource_sumologic_app_test.go
@@ -0,0 +1,156 @@
+package sumologic
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccSumologicApp_basic(t *testing.T) {
+	// uuid of MySql - OpenTelemetry app
+	uuid := "1de46d03-e051-404e-8c6b-53aa7a057df6"
+	version := "1.0.5"
+	parameterKey := "key"
+	parameterValue := "value"
+
+	tfResourceName := "tf_app_test"
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAppDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSumologicApp(tfResourceName, uuid, version, parameterKey, parameterValue),
+			},
+			{
+				ResourceName:      fmt.Sprintf("sumologic_app.%s", tfResourceName),
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccSumologicApp_create(t *testing.T) {
+	// create config
+	// uuid of MySql - OpenTelemetry app
+	uuid := "1de46d03-e051-404e-8c6b-53aa7a057df6"
+	version := "1.0.5"
+	parameterKey := "key"
+	parameterValue := "value"
+
+	tfResourceName := "tf_app_test"
+	tfAppResource := fmt.Sprintf("sumologic_app.%s", tfResourceName)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAppDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSumologicApp(tfResourceName, uuid, version, parameterKey, parameterValue),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAppInstanceExists(tfAppResource, t),
+					resource.TestCheckResourceAttr(tfAppResource, "uuid", uuid),
+					resource.TestCheckResourceAttr(tfAppResource, "version", version),
+					resource.TestCheckResourceAttr(tfAppResource, "parameters.%", "1"),
+					resource.TestCheckResourceAttr(tfAppResource, "parameters.key", "value"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccSumologicApp_update(t *testing.T) {
+
+	// create config
+	// uuid of Varnish - OpenTelemetry app
+	uuid := "d2ef33c3-67f2-4438-9124-14a30ec2ecf3"
+	version := "1.0.3"
+	parameterKey := "key"
+	parameterValue := "value"
+
+	tfResourceName := "tf_app_test"
+	tfAppResource := fmt.Sprintf("sumologic_app.%s", tfResourceName)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAppDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSumologicApp(tfResourceName, uuid, version, parameterKey, parameterValue),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAppInstanceExists(tfAppResource, t),
+					resource.TestCheckResourceAttr(tfAppResource, "uuid", uuid),
+					resource.TestCheckResourceAttr(tfAppResource, "version", version),
+					resource.TestCheckResourceAttr(tfAppResource, "parameters.%", "1"),
+					resource.TestCheckResourceAttr(tfAppResource, "parameters.key", "value"),
+				),
+			},
+			{
+				Config: testAccSumologicApp(tfResourceName, uuid, "1.0.4", parameterKey, parameterValue),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAppInstanceExists(tfAppResource, t),
+					resource.TestCheckResourceAttr(tfAppResource, "uuid", uuid),
+					resource.TestCheckResourceAttr(tfAppResource, "version", "1.0.4"),
+					resource.TestCheckResourceAttr(tfAppResource, "parameters.%", "1"),
+					resource.TestCheckResourceAttr(tfAppResource, "parameters.key", "value"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAppDestroy() resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*Client)
+		for _, r := range s.RootModule().Resources {
+			id := r.Primary.ID
+			appInstance, _ := client.GetAppInstance(id)
+			if appInstance != nil {
+				return fmt.Errorf("AppInstance %s still exists", id)
+			}
+		}
+		return nil
+	}
+}
+
+func testAccCheckAppInstanceExists(name string, t *testing.T) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Error = %s. App Instance not found: %s", strconv.FormatBool(ok), name)
+		}
+
+		if strings.EqualFold(rs.Primary.ID, "") {
+			return fmt.Errorf("App Instance ID is not set")
+		}
+
+		id := rs.Primary.ID
+		client := testAccProvider.Meta().(*Client)
+		_, err := client.GetAppInstance(id)
+		if err != nil {
+			return fmt.Errorf("App instance (id=%s) not found", id)
+		}
+		return nil
+	}
+}
+
+func testAccSumologicApp(tfResourceName string, uuid string, version string, parameterKey string, parameterValue string) string {
+
+	return fmt.Sprintf(`
+
+	resource "sumologic_app" "%s" {
+		uuid = "%s"
+		version = "%s"
+		parameters = {
+			"%s": "%s"
+		}
+	}
+	`, tfResourceName, uuid, version, parameterKey, parameterValue)
+}
--- sumologic/sumologic_app.go
+++ sumologic/sumologic_app.go
@@ -26,70 +26,92 @@ func (s *Client) GetAppInstance(id string) (*AppInstance, error) {
 	return &appInstance, nil
 }
 
-func (s *Client) CreateAppInstance(uuid string, appInstallPayload AppInstallPayload) (*AppInstallResponse, error) {
+func (s *Client) CreateAppInstance(uuid string, appInstallPayload AppInstallPayload) (string, error) {
 	url := fmt.Sprintf("v2/apps/%s/install", uuid)
-	jobId, err := s.Post(url, appInstallPayload)
+	response, err := s.Post(url, appInstallPayload)
 	if err != nil {
-		return nil, err
+		return "", err
+	}
+
+	var jobId AppInstallJobId
+	err = json.Unmarshal(response, &jobId)
+	if err != nil {
+		return "", err
 	}
 
 	// Wait for install job to finish
-	url = fmt.Sprintf("v2/apps/install/%s/status", jobId)
+	url = fmt.Sprintf("v2/apps/install/%s/status", jobId.JOBID)
 	_, err = waitForJob(url, time.Minute, s)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	var appInstallResponse AppInstallResponse
 	b, _, _ := s.Get(url)
 	err = json.Unmarshal(b, &appInstallResponse)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	log.Printf("[CreateAppInstance] response: %+v\n", appInstallResponse)
-	return &appInstallResponse, nil
+	return appInstallResponse.INSTANCEID, nil
 }
 
 func (s *Client) DeleteAppInstance(uuid string) error {
 	url := fmt.Sprintf("v2/apps/%s/uninstall", uuid)
-	jobId, err := s.Post(url, nil)
+	response, err := s.Post(url, nil)
+	if err != nil {
+		return err
+	}
+
+	var jobId AppInstallJobId
+	err = json.Unmarshal(response, &jobId)
 	if err != nil {
 		return err
 	}
 
 	// Wait for install job to finish
-	url = fmt.Sprintf("v2/apps/uninstall/%s/status", jobId)
+	url = fmt.Sprintf("v2/apps/uninstall/%s/status", jobId.JOBID)
 	_, err = waitForJob(url, time.Minute, s)
 	return err
 }
 
-func (s *Client) UpdateAppInstance(uuid string, appInstallPayload AppInstallPayload) (*AppInstallResponse, error) {
+func (s *Client) UpdateAppInstance(uuid string, appInstallPayload AppInstallPayload) (string, error) {
 	url := fmt.Sprintf("v2/apps/%s/upgrade", uuid)
-	jobId, err := s.Post(url, appInstallPayload)
+	response, err := s.Post(url, appInstallPayload)
 	if err != nil {
-		return nil, err
+		return "", err
+	}
+
+	var jobId AppInstallJobId
+	err = json.Unmarshal(response, &jobId)
+	if err != nil {
+		return "", err
 	}
 
 	// Wait for install job to finish
-	url = fmt.Sprintf("v2/apps/upgrade/%s/status", jobId)
+	url = fmt.Sprintf("v2/apps/upgrade/%s/status", jobId.JOBID)
 	_, err = waitForJob(url, time.Minute, s)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	var appInstallResponse AppInstallResponse
 	b, _, _ := s.Get(url)
 	err = json.Unmarshal(b, &appInstallResponse)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	log.Printf("[UpdateAppInstance] response: %+v\n", appInstallResponse)
-	return &appInstallResponse, nil
+	return appInstallResponse.INSTANCEID, nil
 }
 
 type AppInstallPayload struct {
-	VERSION    string            `json:"version"`
-	PARAMETERS map[string]string `json:"parameters"`
+	VERSION    string                 `json:"version"`
+	PARAMETERS map[string]interface{} `json:"parameters"`
+}
+
+type AppInstallJobId struct {
+	JOBID string `json:"jobId"`
 }
 
 type AppInstallResponse struct {
@@ -99,26 +121,8 @@ type AppInstallResponse struct {
 }
 
 type AppInstance struct {
-	ID                string          `json:"id"`
-	UUID              string          `json:"uuid"`
-	VERSION           string          `json:"version"`
-	NAME              string          `json:"name"`
-	DESCRIPTION       string          `json:"description"`
-	CONFIGURATIONBLOB string          `json:"configurationBlob"`
-	PREVIOUSVERSION   string          `json:"previousVersion"`
-	LATESTVERSION     string          `json:"latestVersion"`
-	PATH              string          `json:"path"`
-	MANAGEDOBJECTS    []ManagedObject `json:"managedObjects"`
-	FOLDERID          string          `json:"folderId"`
-	CREATEDAT         string          `json:"createdAt"`
-	CREATEDBY         string          `json:"createdBy"`
-	MODIFIEDAT        string          `json:"modifiedAt"`
-	MODIFIEDBY        string          `json:"modifiedBy"`
-}
-
-type ManagedObject struct {
-	ID          string `json:"id"`
-	NAME        string `json:"name"`
-	TYPE        string `json:"type"`
-	DESCRIPTION string `json:"description"`
+	ID                string `json:"id"`
+	UUID              string `json:"uuid"`
+	VERSION           string `json:"version"`
+	CONFIGURATIONBLOB string `json:"configurationBlob"`
 }
