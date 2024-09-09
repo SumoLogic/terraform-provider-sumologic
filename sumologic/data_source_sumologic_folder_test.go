d8dee16a7cfc67f0b051cbc6396c68436112d0fc
Sajal Jain <sjain@sumologic.com>
2024-09-09 10:27:01 -0700

    SUMO-245407: Add support for list apps v2 as data resource

--- sumologic/data_source_sumologic_folder_test.go
+++ sumologic/data_source_sumologic_folder_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestAccDataSourceFolder_basic(t *testing.T) {
@@ -17,29 +16,11 @@ func TestAccDataSourceFolder_basic(t *testing.T) {
 			{
 				Config: folderConfig("/Library/Users/dgould+terraform@sumologic.com"),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckSumoLogicfDataSourceID("data.sumologic_folder.personal_folder"),
 					testAccDataSourceFolderCheck("data.sumologic_folder.personal_folder"),
 				),
 			},
 		},
 	})
-}
-
-func testAccCheckSumoLogicfDataSourceID(n string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("Can't find SumoLogic Apps data source: %s", n)
-		}
-
-		fmt.Printf("%v\n", s.RootModule().Resources)
-		fmt.Printf("%v\n", rs.Primary)
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("SumoLogic Apps data source ID not set")
-		}
-		return nil
-	}
 }
 
 func TestAccDataSourceFolder_folder_does_not_exist(t *testing.T) {
