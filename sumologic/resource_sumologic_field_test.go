e1f5831bf8b0e045b49083dd65d62a6c91886fba
Priyansh Goel <priyansh.goel@sumologic.com>
2025-01-09 21:30:10 +0530

    SUMO-0 add tests

--- sumologic/resource_sumologic_field_test.go
+++ sumologic/resource_sumologic_field_test.go
@@ -128,6 +128,12 @@ func TestAccSumologicFieldUpdate_OnlyStateFieldIsUpdatable(t *testing.T) {
 						field_name = "%s"
 						data_type  = "%s"
 						state      = "%s"
+
+						lifecycle {
+    						ignore_changes = [
+    							data_type 
+    						]
+  						}
 					}
 				`, testFieldName, updatedDataType, testState),
 				ExpectError: regexp.MustCompile("Only state field is updatable"),
