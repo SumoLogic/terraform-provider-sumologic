acbe9a1de536df39e6fdec3f09ad277c0af38bdd
Abhishek Singh <abhisheksingh@prod-www-2105927136.us-east-1.elb.amazonaws.com>
2025-06-18 23:39:28 +0530

    SUMO-254792: Fixing tests

--- sumologic/resource_sumologic_log_search_test.go
+++ sumologic/resource_sumologic_log_search_test.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 	"strings"
 	"testing"
-	"regexp"
+
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
